560e5e6d0f1026d7c0fc8f024bc0b39242d66ef3
thonymg <copycates@gmail.com>
2014-11-12 09:08:51 +0300

    Règlages footer

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <?php
 ?>
 
-	</div><!-- #content -->
+    </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'bemainty' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'bemainty' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'bemainty' ), 'bemainty', '<a href="http://anosibe.wordpress.com" rel="designer">ThonyMg</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-info">
+            <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'bemainty' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'bemainty' ), 'WordPress' ); ?></a>
+            <span class="sep"> | </span>
+            <?php printf( __( 'Theme: %1$s by %2$s.', 'bemainty' ), '<a href="https://github.com/thonymg/bemainty" rel="designer">Bemainty</a>', '<a href="http://neo-outsourcing.businesscatalyst.com/" rel="designer">ThonyMg</a>' ); ?>
+        </div><!-- .site-info -->
+    </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
